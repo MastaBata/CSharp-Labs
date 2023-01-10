01f89d88d395635b6c2ebafd66a2ce4f612277d0
MastaBata <60700819+MastaBata@users.noreply.github.com>
2023-01-10 13:09:39 +0200

    Add files via upload

---
+++ Laba1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static int TryCatchInt(int r)
+        {
+            while (true)
+            {
+                try
+                {
+                    r = int.Parse(Console.ReadLine());
+                    return r;
+                   
+                }
+                catch
+                {
+                    Console.WriteLine("Enter number!");
+                }
+            }
+            double S = 4 * 3.14 * 2 * r;
+            Console.WriteLine("S:");
+            return S;
+            double V = (4 / 3) * 3.14 * (r * r * r);
+            return V;
+        }
+        
+    }
+}
