d6c353dcb9130af654b73bc2cde0a2629437b34a
MastaBata <60700819+MastaBata@users.noreply.github.com>
2023-01-11 16:05:20 +0200

    Add files via upload

---
+++ CSharp-Labs/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter R: ");
+            double R = int.Parse(Console.ReadLine());
+
+            double S = 4 * Math.PI * Math.Pow(R, 2);
+            Console.WriteLine($"Result {S}");
+            double V = (4/3) * Math.PI * Math.Pow(R, 3);
+            Console.WriteLine($"Result {V}");
+        }
+    }
+}
